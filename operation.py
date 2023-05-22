290f5e2b631554aaa24018acb2bba2789721a1c0
ssss <sdevdariani9@gmail.com>
2023-05-22 16:50:00 +0400

    Update train.py

--- operation.py
+++ operation.py
@@ -46,22 +46,23 @@ def load_params(model, new_param):
         p.data.copy_(new_p)
 
 
-def get_dir(args):
-    task_name = 'train_results/' + args.name
-    saved_model_folder = os.path.join( task_name, 'models')
-    saved_image_folder = os.path.join( task_name, 'images')
+def get_dir(args, output_path):
+    task_name = os.path.join(output_path, args.name)
+    saved_model_folder = os.path.join(task_name, 'models')
+    saved_image_folder = os.path.join(task_name, 'images')
     
     os.makedirs(saved_model_folder, exist_ok=True)
     os.makedirs(saved_image_folder, exist_ok=True)
 
     for f in os.listdir('./'):
         if '.py' in f:
-            shutil.copy(f, task_name+'/'+f)
+            shutil.copy(f, os.path.join(task_name, f))
     
-    with open( os.path.join(saved_model_folder, '../args.txt'), 'w') as f:
+    with open(os.path.join(saved_model_folder, '../args.txt'), 'w') as f:
         json.dump(args.__dict__, f, indent=2)
 
     return saved_model_folder, saved_image_folder
+
 
 
 class  ImageFolder(Dataset):
